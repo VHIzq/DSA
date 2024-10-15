7228b74c52919182bb12a411751f589adef87ecf
vhmi <vhmizq@gmail.com>
2024-10-15 14:07:39 -0600

    add logic ex

---
+++ DSA-udemy/1_logic_exercises.js
@@ -0,0 +1,26 @@
+const minus = () => {
+  let a = 5;
+  let b = 1;
+  let result = 0;
+
+  while (a > b) {
+    b++;
+    result++;
+  }
+  console.log('Result: ', result);;
+};
+
+const multiply = (a, b) => {
+  let total = 0;
+  while (b > 0) {
+    b--;
+    total += a;
+  }
+  console.log('Result: ', total);
+}
+
+const cubic = (base, exp) => {
+  total = 0;
+  
+}
+
